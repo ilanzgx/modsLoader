ca7b5edeb58ee1d5715c278f43e4d881ab3843fa
ilanzgx <ilan-_@hotmail.com>
2024-05-02 13:02:04 -0300

    super optimization and colors in the console

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import os
+import colorama
 import dropbox
 import dropbox.files
 import time
+import hashlib
+from colorama import Fore, Style
 
 APP_KEY = ''
 APP_SECRET = ''
 REFRESH_TOKEN = ''
 
+count = 0
+
+colorama.init()
 dbx = dropbox.Dropbox(
     app_key=APP_KEY,
     app_secret=APP_SECRET,
@@ -14,21 +20,51 @@ dbx = dropbox.Dropbox(
 )
 
 start_download_time = time.time()
-def download_all_cloud_files():
-    for entry in dbx.files_list_folder("").entries:
-        file_metadata = dbx.files_get_metadata(f"/{entry.name}")
-        file_size = file_metadata.size / (1024 * 1024)
-        start_time = time.time()
-        print(f"Downloading {entry.name} (Size: {file_size:.2f} MB)...", end=' ')
 
-        dbx.files_download_to_file(os.path.join("mods", entry.name), f"/{entry.name}")
+def dropbox_content_hash(file):
+    hash_chunk_size = 4 * 1024 * 1024;
+    with open(file, "rb") as f:
+        block_hashes = bytes()
+        while True:
+            chunk = f.read(hash_chunk_size)
+            if not chunk:
+                break
+            block_hashes += hashlib.sha256(chunk).digest()
+        return hashlib.sha256(block_hashes).hexdigest()
 
-        end_time = time.time()
-        download_time = end_time - start_time
-        download_speed = file_size / download_time
-        print(f"done. Download time: {download_time:.2f} seconds. Speed: {download_speed:.2f} MB/second.")
+def download_changed():
+    dropbox_entries = dbx.files_list_folder("").entries
+    dropbox_file_names = [entry.name for entry in dropbox_entries]
+    global count
 
-download_all_cloud_files()
+    for entry in dropbox_entries:
+        #file_metadata = dbx.files_get_metadata(f"/{entry.name}")
+        file_size = entry.size / (1024 * 1024)
+        local_file_path = os.path.join("mods", entry.name)
+        count += 1
 
+        if os.path.exists(local_file_path):
+            local_hash = dropbox_content_hash(local_file_path)
+            if local_hash != entry.content_hash:
+                # CHANGES
+                print(f"{Style.BRIGHT}{Fore.YELLOW}MOD FOI ATUALIZADO: {entry.name} {Fore.WHITE}[{file_size:0.2f} MB]")
+                dbx.files_download_to_file(local_file_path, f"/{entry.name}")
+            else:
+                # NO CHANGES
+                print(f"{Style.BRIGHT}{Fore.CYAN}SEM MUDANÇAS: {entry.name} {Fore.WHITE}[{file_size:0.2f} MB]")
+        else:
+            # NEW FILES
+            print(f"{Style.BRIGHT}{Fore.GREEN}NOVO MOD: {entry.name} {Fore.WHITE}[{file_size:0.2f} MB]")
+            dbx.files_download_to_file(local_file_path, f"/{entry.name}")
+
+    local_files = os.listdir("mods")
+    for local_file in local_files:
+        if local_file not in dropbox_file_names:
+            # DELETE FILE
+            print(f"{Style.BRIGHT}{Fore.RED}MOD REMOVIDO: {local_file} {Fore.WHITE}[{file_size:0.2f} MB]")
+            os.remove(os.path.join("mods", local_file))
+
+download_changed()
 end_download_time = time.time()
-print(f"Time: {end_download_time - start_download_time:.2f} seconds.")
+print(f"\n{Style.BRIGHT}Tempo: {end_download_time - start_download_time:.2f} Segundos.\n{count} Mods instanciados e verificados")
+os.system("pause")
